e9b80423298087efa80aed018b04e3e2b5d07ba7
Vladlena Tsenzeria <tsenzeria.v@gmail.com>
2025-11-13 19:52:15 +0200

    update

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,34 @@
 "use client";
+import styles from "@/app/styles/components/Footer.module.scss";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="text-center py-6 border-t text-gray-500 text-sm bg-white">
-      © {new Date().getFullYear()} Wszelkie prawa zastrzeżone
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.footer__inner}>
+          <div className={styles.footer__col}>
+            <p>Wszelkie prawa zastrzeżone © {currentYear}</p>
+            <p>Finrekin spółka z ograniczoną odpowiedzialnością</p>
+          </div>
+
+          <div className={styles.footer__col}>
+            <p>ul. Parkowa 25 lok. 58</p>
+            <p>51-616 Wrocław</p>
+          </div>
+
+          <div className={styles.footer__col}>
+            <p>NIP 8982310567</p>
+            <p>REGON 529341562</p>
+            <p>Kapitał zakładowy 5000 zł</p>
+          </div>
+
+          <div className={styles.footer__links}>
+            <a href="mailto:tsenzeria.v@gmail.com">Tworzenie stron</a>
+            <a href="#">Polityka prywatności</a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
